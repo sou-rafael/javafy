04147ae98b500a42198d65cabfe38a83bd2f4770
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-06-25 15:59:26 -0300

    classes User, Artista e Ouvinte

--- src/models/Ouvinte.java
+++ src/models/Ouvinte.java
@@ -1,4 +1,26 @@
 package models;
 
-public class Ouvinte {
+import abstracts.Conta;
+
+public class Ouvinte extends User{
+    private List<Conta> playlists;
+
+    public List<Conta> getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(List<Conta> playlists) {
+        this.playlists = playlists;
+    }
+
+    @Override
+    public String toString() {
+        return "Ouvinte{" +
+                "playlists=" + playlists +
+                '}';
+    }
+
+    public boolean criarPlayList(PlayList) {
+        return;
+    }
 }

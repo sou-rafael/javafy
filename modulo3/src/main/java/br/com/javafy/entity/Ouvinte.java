12d39f8d625e64aff56d0e1c1b08955f162622fc
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2022-07-10 17:06:12 -0300

    Adicionando as classes usuario, e ouvinte nas entidades

---
+++ modulo3/src/main/java/br/com/javafy/entity/Ouvinte.java
@@ -0,0 +1,15 @@
+package br.com.javafy.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Ouvinte extends Usuario{
+
+    private Integer idOuvinte;
+
+}
---
+++ modulo3/src/main/java/br/com/javafy/entity/Usuario.java
@@ -0,0 +1,39 @@
+package br.com.javafy.entity;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public abstract class Usuario {
+
+    private Integer idUser;
+    @NotBlank(message = "Nome não pode ser nulo")
+    private String nome;
+    @NotNull
+    @Past
+    private LocalDate dataNascimento;
+
+    private String genero;
+    @NotNull
+    private Integer premium;
+
+    @Email
+    @NotBlank(message = "Email é obrigatorio")
+    private String email;
+
+    private List<Usuario> seguidores = new ArrayList<>();
+    private List<Usuario> seguindo = new ArrayList<>();
+
+}
